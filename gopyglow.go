91730c882911ce261bfda0448098c331c357fbb1
Johannes Boyne <johannes@boyne.de>
2014-12-14 09:48:21 +0100

    Fix typo

--- gopyglow.go
+++ gopyglow.go
@@ -30,33 +30,38 @@ func (g *Glow) Green(brightness uint8) {
 	g.p.SetLED(3, brightness)
 	g.p.SetLED(5, brightness)
 	g.p.SetLED(13, brightness)
+	g.TrySet()
 }
 
 func (g *Glow) Red(brightness uint8) {
 	g.p.SetLED(0, brightness)
 	g.p.SetLED(6, brightness)
 	g.p.SetLED(17, brightness)
+	g.TrySet()
 }
 
 func (g *Glow) Orange(brightness uint8) {
 	g.p.SetLED(2, brightness)
 	g.p.SetLED(8, brightness)
 	g.p.SetLED(15, brightness)
+	g.TrySet()
 }
 
 func (g *Glow) Blue(brightness uint8) {
 	g.p.SetLED(4, 255)
 	g.p.SetLED(14, 255)
 	g.p.SetLED(11, 255)
+	g.TrySet()
 }
 
 func (g *Glow) Clear() {
 	for i := 0; i < 18; i++ {
 		g.p.SetLED(int8(i), 0)
 	}
+	g.TrySet()
 }
 
 func main() {
-	clear(p)
-
+	glower := Glow.NewGlow()
+	glower.Green(100)
 }
